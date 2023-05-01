999e90a82624070fb58d991c0dbcf33e1418eeb9
Milad Tehrani <tehranimilad92@gmail.com>
2023-05-01 14:48:41 -0700

    deleting code that isn't required

--- MDB5-REACT-UI-KIT-Free-6.0.0/src/App.js
+++ MDB5-REACT-UI-KIT-Free-6.0.0/src/App.js
@@ -36,31 +36,32 @@ export default function App() {
               </p>
             </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+            {/* <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Skills</h6>
+              
               <p>
-                <a href='#!' className='text-reset'>
+                <a className='text-reset'>
                   Angular
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
+                <a className='text-reset'>
                   React
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Vue
+                <a className='text-reset'>
+                  HTML
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
+                <a className='text-reset'>
+                  CSS
                 </a>
               </p>
-            </MDBCol>
+            </MDBCol> */}
 
-            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+            {/* <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
               <p>
                 <a href='#!' className='text-reset'>
@@ -82,7 +83,7 @@ export default function App() {
                   Help
                 </a>
               </p>
-            </MDBCol>
+            </MDBCol> */}
 
             <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
               <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
