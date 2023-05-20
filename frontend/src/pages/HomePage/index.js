75484fc4d8860bfe22728e1e69d2bf56c6841f6c
Milad Tehrani <tehranimilad92@gmail.com>
2023-05-19 23:43:28 -0700

    updated layout of homepage

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -12,9 +12,7 @@ return (
     <h1 className='mb-6'>Hi, nice to meet you!</h1>
     <h2 className='mb-3'>My name is Milad Tehrani</h2>
     <h4 className='mb-3'>I'm a Software Engineer</h4>
-    </div>
     <div class="text-center">
-        <MDBCard background='dark' className='text-white'>
         <MDBCardImage position='top' className="avatar" src={headshot}></MDBCardImage>
           <MDBCardBody>
             I am a highly skilled software engineer with a background in Management Information Systems, dedicated to delivering innovative solutions that improve business processes and drive results. With a passion for technology, I leverage my expertise to solve complex problems and help businesses achieve their goals. 
@@ -25,8 +23,8 @@ return (
           <h1>
           <MDBIcon fab icon="css3" /> <MDBIcon fab icon="css3-alt" /> <MDBIcon fas icon="database" /> <MDBIcon fab icon="github-square" /> <MDBIcon fab icon="html5" /> <MDBIcon fab icon="js" /> <MDBIcon fab icon="node" /> <MDBIcon fab icon="npm" /> <MDBIcon fab icon="python" /> <MDBIcon fab icon="react" /> <MDBIcon fab icon="sass" /> <MDBIcon fab icon="wordpress" /> <MDBIcon fab icon="bootstrap" /> 
           </h1>
-        </MDBCard>
         </div>
+    </div>
     </div>
     </div>
 
