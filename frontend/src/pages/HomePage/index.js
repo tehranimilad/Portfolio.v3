a3784286140368e2999a8ba602986fc941d8053d
Milad Tehrani <tehranimilad92@gmail.com>
2023-08-31 15:26:42 -0700

    added class to home page in order to style badges

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -20,7 +20,7 @@ return (
           <MDBCardTitle>
             Skills
           </MDBCardTitle>
-          <h1>
+          <h1 className="icons">
           <MDBIcon fab icon="css3" /> <MDBIcon fab icon="css3-alt" /> <MDBIcon fas icon="database" /> <MDBIcon fab icon="github-square" /> <MDBIcon fab icon="html5" /> <MDBIcon fab icon="js" /> <MDBIcon fab icon="node" /> <MDBIcon fab icon="npm" /> <MDBIcon fab icon="python" /> <MDBIcon fab icon="react" /> <MDBIcon fab icon="sass" /> <MDBIcon fab icon="wordpress" /> <MDBIcon fab icon="bootstrap" /> 
           </h1>
         </div>
