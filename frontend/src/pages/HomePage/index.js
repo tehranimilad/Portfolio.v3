f8327e3fe6dbe942c08729cb6879278a6f231972
Milad Tehrani <tehranimilad92@gmail.com>
2023-09-05 21:11:59 -0700

    optimizing home page

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -17,10 +17,8 @@ return (
           <MDBCardBody className="aboutDescription">
             I am a highly skilled software engineer with a background in Management Information Systems, dedicated to delivering innovative solutions that improve business processes and drive results. With a passion for technology, I leverage my expertise to solve complex problems and help businesses achieve their goals. 
           </MDBCardBody>
-          <MDBCardTitle>
-            Skills
-          </MDBCardTitle>
           <h1 className="icons">
+            <h2>Skills</h2>
           <MDBIcon fab icon="css3" /> <MDBIcon fab icon="css3-alt" /> <MDBIcon fas icon="database" /> <MDBIcon fab icon="github-square" /> <MDBIcon fab icon="html5" /> <MDBIcon fab icon="js" /> <MDBIcon fab icon="node" /> <MDBIcon fab icon="npm" /> <MDBIcon fab icon="python" /> <MDBIcon fab icon="react" /> <MDBIcon fab icon="sass" /> <MDBIcon fab icon="wordpress" /> <MDBIcon fab icon="bootstrap" /> 
           </h1>
         </div>
