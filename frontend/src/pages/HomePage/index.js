322fb40bb6f0f88acce64c05e775a621eb43c7bd
Milad Tehrani <tehranimilad92@gmail.com>
2023-09-27 17:15:27 -0700

    creating js for animation

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -10,31 +10,52 @@ function HomePage() {
     var textWrapper = document.querySelector('.animationHead .letters');
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-    anime.timeline({ loop: true })
-      .add({
-        targets: '.animationHead .letter',
-        scale: [0.3, 1],
-        opacity: [0, 1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 600,
-        delay: (el, i) => 70 * (i + 1)
-      }).add({
-        targets: '.animationHead .line',
-        scaleX: [0, 1],
-        opacity: [0.5, 1],
-        easing: "easeOutExpo",
-        duration: 700,
-        offset: '-=875',
-        delay: (el, i, l) => 80 * (l - i)
-      }).add({
-        targets: '.animationHead',
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 1000
-      });
-  }, []);
+    anime.timeline({loop: true})
+    .add({
+      targets: '.ml8 .circle-white',
+      scale: [0, 3],
+      opacity: [1, 0],
+      easing: "easeInOutExpo",
+      rotateZ: 360,
+      duration: 1100
+    }).add({
+      targets: '.ml8 .circle-container',
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeInOutExpo",
+      offset: '-=1000'
+    }).add({
+      targets: '.ml8 .circle-dark',
+      scale: [0, 1],
+      duration: 1100,
+      easing: "easeOutExpo",
+      offset: '-=600'
+    }).add({
+      targets: '.ml8 .letters-left',
+      scale: [0, 1],
+      duration: 1200,
+      offset: '-=550'
+    }).add({
+      targets: '.ml8 .bang',
+      scale: [0, 1],
+      rotateZ: [45, 15],
+      duration: 1200,
+      offset: '-=1000'
+    }).add({
+      targets: '.ml8',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1400
+    });
+  
+  anime({
+    targets: '.ml8 .circle-dark-dashed',
+    rotateZ: 360,
+    duration: 8000,
+    easing: "linear",
+    loop: true
+  });
 
   return (
     <div className="homePageDiv">
