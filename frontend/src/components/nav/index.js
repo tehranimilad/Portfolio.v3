917cb4daa4bdc7709247ee9906a354de8fea5f1f
Milad Tehrani <tehranimilad92@gmail.com>
2023-10-09 15:07:01 -0700

    updated navbar

--- frontend/src/components/nav/index.js
+++ frontend/src/components/nav/index.js
@@ -16,18 +16,6 @@ export default function Nav() {
 
 
   return (
-    // <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    //     <div class="collapse navbar-collapse" id="navbarNav"></div>
-    //   <MDBContainer fluid>
-    //     <MDBNavbarBrand>Milad Tehrani</MDBNavbarBrand>
-    //       <MDBNavbarNav>
-    //         <MDBNavbarLink href='#about-me'>About Me</MDBNavbarLink>
-    //         <MDBNavbarLink href='#projects-section'>Projects</MDBNavbarLink>
-    //         <MDBNavbarLink href='#connect-with-me'>Connect With Me</MDBNavbarLink>
-    //         <MDBNavbarLink aria-current='page' href='https://docs.google.com/document/d/1TKMFQ6sR580VwZ-Xmw47L4l11jQ1oLBa0Fa7631GEz8/edit?usp=sharing' target="blank">Resume</MDBNavbarLink>
-    //       </MDBNavbarNav>
-    //   </MDBContainer>
-    // </nav>
     <MDBNavbar expand='lg' light bgColor='light'>
     <MDBContainer fluid>
       <MDBNavbarBrand href='#'>Milad Tehrani</MDBNavbarBrand>
@@ -42,20 +30,15 @@ export default function Nav() {
       <MDBCollapse navbar show={showNav}>
         <MDBNavbarNav>
           <MDBNavbarItem>
-            <MDBNavbarLink active aria-current='page' href='#'>
-              Home
+            <MDBNavbarLink active aria-current='page' href='#about-me'>
+              About Me
             </MDBNavbarLink>
           </MDBNavbarItem>
           <MDBNavbarItem>
-            <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+            <MDBNavbarLink href='#projects-section'>Projects</MDBNavbarLink>
           </MDBNavbarItem>
           <MDBNavbarItem>
-            <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
-          </MDBNavbarItem>
-          <MDBNavbarItem>
-            <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-              Disabled
-            </MDBNavbarLink>
+            <MDBNavbarLink href='#connect-with-me'>Connect With Me</MDBNavbarLink>
           </MDBNavbarItem>
         </MDBNavbarNav>
       </MDBCollapse>
