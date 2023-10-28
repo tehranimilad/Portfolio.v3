6ebcbb4d15ac117e55b47e6f1c468dfb068f98a1
Milad Tehrani <tehranimilad92@gmail.com>
2023-10-27 20:43:01 -0700

    cleaing up the code and changed font size for homepage

--- frontend/src/pages/Projects/index.js
+++ frontend/src/pages/Projects/index.js
@@ -41,44 +41,44 @@ function Projects() {
   }, []);
   return (
     <div className="projectDiv">
-    <div className="text-center">
-    <MDBRow>
-    <h1 class="ml11">
-      <span class="text-wrapper">
-      <span class="line line1"></span>
-      <span class="letters">Projects</span>
-      </span>
-    </h1>
+      <div className="text-center">
+      <MDBRow>
+      <h1 class="ml11">
+        <span class="text-wrapper">
+        <span class="line line1"></span>
+        <span class="letters">Projects</span>
+        </span>
+      </h1>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
-      <MDBCol sm='6'>
-        <MDBCard>
-          <MDBCardBody class="dark">
-          <img className="img-fluid" src={cruiseAmerica}></img>
-            <MDBCardTitle>Cruise America</MDBCardTitle>
-            <MDBCardText>
-            MEN-stack app with full CRUD functionality using Javascript on Node.js and Express frameworks; data stored using MongoDB database through Mongoose, and used EJS partials
-                Utilized Railway to deploy a full stack app; two models with full CRUD functionality
-                Created wireframes and drafted user stories using Wireframe Pro.
-            </MDBCardText>
-            <MDBBtn className="cruiseAmerica-link" href='https://cruise-north-america.herokuapp.com/' target="_blank">Go to Cruise America</MDBBtn>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol sm='6'>
-        <MDBCard>
-          <MDBCardBody>
-          <img class="img-fluid" src={cruiseAmerica2}></img>
-            <MDBCardTitle>Cryptosis</MDBCardTitle>
-            <MDBCardText>
-            MERN-stack app with full CRUD functionality using Javascript on Node.js,Express, and React frameworks; relational database stored using MongoDB database through Mongoose. Cryptosis uses third-party API's in order to retrieve Cryptocurrency relevant data. 
-            </MDBCardText>
-            <MDBBtn className="cryptosis-link" href='https://cryptosis-app.herokuapp.com/' target="_blank">Go to Cryptosis</MDBBtn>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
-    </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+        <MDBCol sm='6'>
+          <MDBCard>
+            <MDBCardBody class="dark">
+            <img className="img-fluid" src={cruiseAmerica}></img>
+              <MDBCardTitle>Cruise America</MDBCardTitle>
+              <MDBCardText>
+              MEN-stack app with full CRUD functionality using Javascript on Node.js and Express frameworks; data stored using MongoDB database through Mongoose, and used EJS partials
+                  Utilized Railway to deploy a full stack app; two models with full CRUD functionality
+                  Created wireframes and drafted user stories using Wireframe Pro.
+              </MDBCardText>
+              <MDBBtn className="cruiseAmerica-link" href='https://cruise-north-america.herokuapp.com/' target="_blank">Go to Cruise America</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol sm='6'>
+          <MDBCard>
+            <MDBCardBody>
+            <img class="img-fluid" src={cruiseAmerica2}></img>
+              <MDBCardTitle>Cryptosis</MDBCardTitle>
+              <MDBCardText>
+              MERN-stack app with full CRUD functionality using Javascript on Node.js,Express, and React frameworks; relational database stored using MongoDB database through Mongoose. Cryptosis uses third-party API's in order to retrieve Cryptocurrency relevant data. 
+              </MDBCardText>
+              <MDBBtn className="cryptosis-link" href='https://cryptosis-app.herokuapp.com/' target="_blank">Go to Cryptosis</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+      </div>
     </div>
   );
 }
